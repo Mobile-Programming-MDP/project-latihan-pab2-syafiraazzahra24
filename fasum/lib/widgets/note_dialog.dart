a8eb7e6bb5fe58310fe3ed5dcfa1f0ee55abbd95
jawa <syafiraazzahra_2226240082@mhs.mdp.ac.id>
2025-04-23 18:36:50 +0700

    a

---
+++ fasum/lib/widgets/note_dialog.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:fasum/models/note.dart';
+import 'package:fasum/services/note_service.dart';
+
+class NoteDialog extends StatefulWidget {
+  final Note? note;
+
+  const NoteDialog({super.key, this.note})
+
+  @override
+  State<NoteDialog
+}
